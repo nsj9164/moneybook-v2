99a327e59a2d8013f0f872fd07d05bceb5b76aa2
nsj9164 <nsj9164@gmail.com>
2025-04-18 13:46:30 +0900

    🚨fix: 설치 누락된 패키지 및 경로 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { HeroSection } from "./components/sections/HeroSection";
 import { AuthSection } from "./components/sections/AuthSection";
 
 function App() {
-  // return <SplitLayout left={<HeroSection />} right={<AuthSection />} />
-  return (
+  return <SplitLayout left={<HeroSection />} right={<AuthSection />} />;
+  /* return (
     <div className="text-3xl text-green-500 font-bold p-10">
       ✅ Tailwind 적용됨!
     </div>
-  );
+  ); */
 }
 
 export default App;
