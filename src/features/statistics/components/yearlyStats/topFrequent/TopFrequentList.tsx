fc86bebd8bd0f43854d348ef96144c96d61dddc9
nsj9164 <nsj9164@gmail.com>
2025-08-27 23:36:48 +0900

    fix: 폴더명 수정

---
+++ src/features/statistics/components/yearlyStats/topFrequent/TopFrequentList.tsx
@@ -0,0 +1,15 @@
+import { CardSection } from "@/components/common/layout/CardSection";
+import { TopItem } from "../../../types/YearlyStatisticsType";
+import { TopFrequentItem } from "./TopFrequentItem";
+
+export const TopFrequentList = ({ topItems }: { topItems: TopItem[] }) => {
+  return (
+    <CardSection title="소비가 잦은 항목 TOP 3">
+      <div className="space-y-4">
+        {topItems.map((item, index) => (
+          <TopFrequentItem key={item.itemName} topItem={item} index={index} />
+        ))}
+      </div>
+    </CardSection>
+  );
+};
